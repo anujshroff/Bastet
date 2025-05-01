d615551000a7990abcaeafadf4a6b3430df57eec
Anuj Shroff <moonlight82@gmail.com>
2025-05-01 16:41:07 -0500

    Use SQLite for in memory database for tests

--- test/Bastet.Tests/TestHelpers/TestDbContextFactory.cs
+++ test/Bastet.Tests/TestHelpers/TestDbContextFactory.cs
@@ -1,37 +1,41 @@
 using Bastet.Data;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bastet.Tests.TestHelpers;
 
 /// <summary>
-/// Factory for creating test database contexts using an in-memory database
+/// Factory for creating test database contexts using an SQLite in-memory database
 /// </summary>
 public static class TestDbContextFactory
 {
     /// <summary>
-    /// Creates a new in-memory database context with a unique database name
+    /// Creates a new SQLite in-memory database context
     /// </summary>
     /// <returns>A configured DbContext for testing</returns>
     public static BastetDbContext CreateDbContext()
     {
-        // Create a unique name for the in-memory database
-        string dbName = $"BastetTestDb_{Guid.NewGuid()}";
+        // Create SQLite connection open to in-memory database
+        SqliteConnection connection = new("DataSource=:memory:");
+        connection.Open();
 
         // Set up the service collection
         ServiceCollection services = new();
 
-        // Add an in-memory database with transaction warnings suppressed
+        // Add an SQLite database using the open connection
         services.AddDbContext<BastetDbContext>(options =>
-            options.UseInMemoryDatabase(dbName)
-                   .ConfigureWarnings(warnings =>
-                       warnings.Ignore(InMemoryEventId.TransactionIgnoredWarning)));
+            options.UseSqlite(connection));
 
         // Build the service provider
         ServiceProvider serviceProvider = services.BuildServiceProvider();
 
         // Get the context from the service provider
-        return serviceProvider.GetRequiredService<BastetDbContext>();
+        BastetDbContext context = serviceProvider.GetRequiredService<BastetDbContext>();
+
+        // Create the schema in the in-memory database
+        context.Database.EnsureCreated();
+
+        return context;
     }
 }
